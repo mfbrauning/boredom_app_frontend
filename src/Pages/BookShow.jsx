b4b819964924b306d0a8776f5ae317e30299cbb2
faith musa <faithy@faiths-MacBook-Air.local>
2021-11-17 17:53:27 -0600

    snacks feature done

--- src/Pages/BookShow.jsx
+++ src/Pages/BookShow.jsx
@@ -1,96 +1,135 @@
-import {useEffect, useState} from "react"
-import {useParams, useNavigate} from "react-router-dom"
+import { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
 
+function BookShow(props) {
+  const params = useParams();
+  const navigate = useNavigate();
+  const id = params.id;
+  const books = props.books;
+  const snacks = props.snacks;
+  const [editForm, setEditForm] = useState({});
 
-function BookShow(props){
-    const params = useParams()
-    const navigate = useNavigate()
-    const id = params.id
-    const books = props.books
-    const [editForm, setEditForm] = useState({})
-    useEffect(() => {
-        if(props.books){
-            const book = books.find((b) => b._id === id)
-            setEditForm(book)
-        }
-    }, [props.books])
-
-    if (props.books){
-        const book = books.find((b) => b._id === id)
-
-        const handleChange = (event) => {
-            const newState = {...editForm}
-            newState[event.target.name] = event.target.value 
-            setEditForm(newState)
-        }
-
-        const handleSubmit = (event) => {
-            event.preventDefault()
-            props.updateBooks(editForm, book._id)
-            navigate("/books")
-        }
-
-        const removeBooks = () => {
-            props.deleteBooks(book._id)
-            navigate("/books")
-        }
-
-        const form = (
-        <form onSubmit={handleSubmit}>
-        <input
-        type="text"
-        value={editForm.title}
-        name="title"
-        placeholder="title"
-        onChange={handleChange}/>
-         <input
-        type="text"
-        value={editForm.author}
-        name="author"
-        placeholder="author"
-        onChange={handleChange}/>
-         <input
-        type="text"
-        value={editForm.year}
-        name="year"
-        placeholder="year"
-        onChange={handleChange}/>
-         <input
-        type="text"
-        value={editForm.genre}
-        name="genre"
-        placeholder="genre"
-        onChange={handleChange}/>
-         <input
-        type="text"
-        value={editForm.image}
-        name="image"
-        placeholder="image"
-        onChange={handleChange}/>
-         <input
-        type="text"
-        value={editForm.link}
-        name="link"
-        placeholder="link"
-        onChange={handleChange}/>
-        <input type="submit" value="Update Book"/>
-        </form>
-        )
-
-        return (
-            <div className="book">
-                <h1>{book.title}</h1>
-                <img src={book.image} alt={book.title}/>
-                {form}<br/>
-                <button onClick={removeBooks}>
-                    Delete Book
-                </button>
-            </div>
-        )
-    } else {
-        return <h1>No Book Found</h1>
+  useEffect(() => {
+    if (props.books) {
+      const book = books.find((b) => b._id === id);
+      setEditForm(book);
     }
+  }, [props.books]);
+  //display snacks
+  const newSnack = () => {
+    if (snacks) {
+      const getSnack = () => {
+        for (const snack of props.snacks.recipes[0].analyzedInstructions[0]
+          .steps) {
+          console.log(snack.step);
+          return snack.step;
+        }
+      };
 
+      return (
+        <div className="snacks">
+          <h2>{snacks.recipes[0].title}</h2>
+          <img src={snacks.recipes[0].image} />
+          <br />
+
+          <p>
+            <ul>
+              {props.snacks.recipes[0].analyzedInstructions[0].steps.map(
+                (step) => {
+                  return <li> {step.step}</li>;
+                }
+              )}
+            </ul>
+          </p>
+        </div>
+      );
+    }
+  };
+
+  if (props.books) {
+    const book = books.find((b) => b._id === id);
+
+    const handleChange = (event) => {
+      const newState = { ...editForm };
+      newState[event.target.name] = event.target.value;
+      setEditForm(newState);
+    };
+
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      props.updateBooks(editForm, book._id);
+      navigate("/books");
+    };
+
+    const removeBooks = () => {
+      props.deleteBooks(book._id);
+      navigate("/books");
+    };
+
+    const form = (
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={editForm.title}
+          name="title"
+          placeholder="title"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          value={editForm.author}
+          name="author"
+          placeholder="author"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          value={editForm.year}
+          name="year"
+          placeholder="year"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          value={editForm.genre}
+          name="genre"
+          placeholder="genre"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          value={editForm.image}
+          name="image"
+          placeholder="image"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          value={editForm.link}
+          name="link"
+          placeholder="link"
+          onChange={handleChange}
+        />
+        <input type="submit" value="Update Book" />
+      </form>
+    );
+
+    return (
+      <>
+        <div className="book">
+          <h1>{book.title}</h1>
+          <img src={book.image} alt={book.title} />
+        </div>
+        <section> {newSnack()}</section>
+        <section>
+          {form} <br />
+          <button onClick={removeBooks}>Delete Book</button>
+        </section>
+      </>
+    );
+  } else {
+    return <h1>No Book Found</h1>;
+  }
 }
 
-export default BookShow
+export default BookShow;
--- src/Pages/MovieShow.jsx
+++ src/Pages/MovieShow.jsx
@@ -1,121 +1,156 @@
 import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 
-function MovieShow(props){
-    const params = useParams()
-    const navigate = useNavigate()
-     const id = params.id
-     const movies = props.movies
-     const [editForm, setEditForm] = useState({})
+function MovieShow(props) {
+  const params = useParams();
+  const navigate = useNavigate();
+  const id = params.id;
+  const movies = props.movies;
+  const [editForm, setEditForm] = useState({});
+  const snacks = props.snacks;
 
-     useEffect(() => {
-         if(props.movies){
-             const movie = movies.find((m) => m._id === id)
-             setEditForm(movie)
-         }
-     }, [props.movies])
-
-     if(props.movies){
-        const movie = movies.find((m) => m._id === id)
-
-        const handleChange = (event) => {
-            const newState = {...editForm}
-            newState[event.target.name] = event.target.value 
-            setEditForm(newState)
+  useEffect(() => {
+    if (props.movies) {
+      const movie = movies.find((m) => m._id === id);
+      setEditForm(movie);
+    }
+  }, [props.movies]);
+  const newSnack = () => {
+    if (snacks) {
+      const getSnack = () => {
+        for (const snack of props.snacks.recipes[0].analyzedInstructions[0]
+          .steps) {
+          console.log(snack.step);
+          return snack.step;
         }
+      };
 
-        const handleSubmit = (event) => {
-            event.preventDefault()
-            props.updateMovie(editForm, movie._id)
-            navigate("/")
-        }
+      return (
+        <div className="snacks">
+          <h2>{snacks.recipes[0].title}</h2>
+          <img src={snacks.recipes[0].image} />
+          <br />
 
-        const removeMovie = () => {
-            props.deleteMovie(movie._id)
-            navigate("/")
-        }
+          <p>
+            <ul>
+              {props.snacks.recipes[0].analyzedInstructions[0].steps.map(
+                (step) => {
+                  return <li> {step.step}</li>;
+                }
+              )}
+            </ul>
+          </p>
+        </div>
+      );
+    }
+  };
 
-        const form = (
-            <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        value={editForm.title}
-        name="title"
-        placeholder="Title"
-        onChange={handleChange}
-      />
-      <input
-        type="text"
-        value={editForm.director}
-        name="director"
-        placeholder="Director"
-        onChange={handleChange}
-      />
-      <input
-        type="text"
-        value={editForm.year}
-        name="year"
-        placeholder="Year"
-        onChange={handleChange}
-      />
-      <input
-        type="text"
-        value={editForm.genre}
-        name="genre"
-        placeholder="Genre"
-        onChange={handleChange}
-      />
-      <input
-        type="text"
-        value={editForm.image}
-        name="image"
-        placeholder="Image URL"
-        onChange={handleChange}
-      />
-      <input
-        type="text"
-        value={editForm.rating}
-        name="rating"
-        placeholder="Rating"
-        onChange={handleChange}
-      />
-      <input
-        type="text"
-        value={editForm.link}
-        name="link"
-        placeholder="Link to 3rd party Movie description"
-        onChange={handleChange}
-      />
-      <input
-        type="text"
-        value={editForm.video}
-        name="video"
-        placeholder="Link Movie Trailer"
-        onChange={handleChange}
-      />
-      <input type="submit" value="Update Movie" />
-    </form>
-        )
+  if (props.movies) {
+    const movie = movies.find((m) => m._id === id);
+
+    const handleChange = (event) => {
+      const newState = { ...editForm };
+      newState[event.target.name] = event.target.value;
+      setEditForm(newState);
+    };
+
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      props.updateMovie(editForm, movie._id);
+      navigate("/");
+    };
+
+    const removeMovie = () => {
+      props.deleteMovie(movie._id);
+      navigate("/");
+    };
+
+    const form = (
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={editForm.title}
+          name="title"
+          placeholder="Title"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          value={editForm.director}
+          name="director"
+          placeholder="Director"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          value={editForm.year}
+          name="year"
+          placeholder="Year"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          value={editForm.genre}
+          name="genre"
+          placeholder="Genre"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          value={editForm.image}
+          name="image"
+          placeholder="Image URL"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          value={editForm.rating}
+          name="rating"
+          placeholder="Rating"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          value={editForm.link}
+          name="link"
+          placeholder="Link to 3rd party Movie description"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          value={editForm.video}
+          name="video"
+          placeholder="Link Movie Trailer"
+          onChange={handleChange}
+        />
+        <input type="submit" value="Update Movie" />
+      </form>
+    );
 
     return (
+      <>
         <div>
-            <h1>Title: {movie.title}</h1>
-            <h3>Director: {movie.director}</h3>
-              <h3>Year: {movie.year}</h3>
-              <h3>Genre: {movie.genre}</h3>
-              <h3>Rating: {movie.rating}</h3>
-              <h3>IMDB: {movie.link}</h3>
-              <img src={movie.image} />
-              <iframe width="100%" height="820" src={movie.video}></iframe>
-            {form}
-            <button id="delete" onClick={removeMovie}>
-                Delete Movie
-            </button>
+          <h1>Title: {movie.title}</h1>
+          <h3>Director: {movie.director}</h3>
+          <h3>Year: {movie.year}</h3>
+          <h3>Genre: {movie.genre}</h3>
+          <h3>Rating: {movie.rating}</h3>
+          <h3>IMDB: {movie.link}</h3>
+          <img src={movie.image} />
+          <iframe width="100%" height="820" src={movie.video}></iframe>
         </div>
-    )
-     }else{
-        return <h1>No Movie found</h1>
-     }
+        <section> {newSnack()}</section>
+        <section>
+          {form}
+          <button id="delete" onClick={removeMovie}>
+            Delete Movie
+          </button>
+        </section>
+      </>
+    );
+  } else {
+    return <h1>No Movie found</h1>;
+  }
 }
 
-export default MovieShow
+export default MovieShow;
